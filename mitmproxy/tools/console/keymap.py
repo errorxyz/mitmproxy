41aa0c14e78a2f90bfb398157b058e349e0f8355
errorxyz <gjain1563@gmail.com>
2025-01-09 01:34:17 +0530

    Revert "Prompt before executing shortcuts mapped to destructive commands"
    This reverts commit 45f609ad3edd18458d97ad99e9531d88bcc33fc9.

--- mitmproxy/tools/console/keymap.py
+++ mitmproxy/tools/console/keymap.py
@@ -86,14 +86,11 @@ class Keymap:
         signals.keybindings_change.send()
         self.binding_for_help.cache_clear()
 
-    def add(self, key: str, command: str, contexts: Sequence[str], help="", prompt: bool = False) -> None:
+    def add(self, key: str, command: str, contexts: Sequence[str], help="") -> None:
         """
         Add a key to the key map.
         """
         self._check_contexts(contexts)
-
-        if prompt:
-            command = f"console.command.confirm \"{help}\" {command.strip()}"
 
         for b in self.bindings:
             if b.key == key and b.command.strip() == command.strip():
